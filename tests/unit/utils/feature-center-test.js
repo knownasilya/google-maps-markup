937fd226bf740c8cf75dab855290e87523049aaf
Ilya Radchenko <ilya@burstcreations.com>
2015-07-28 14:49:42 -0400

    Feature center and measurement tests

--- tests/unit/utils/feature-center-test.js
+++ tests/unit/utils/feature-center-test.js
@@ -3,8 +3,7 @@ import { module, test } from 'qunit';
 
 module('Unit | Utility | feature center');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
+test('point', function(assert) {
   var feature = new google.maps.Data.Feature({
     geometry: new google.maps.Data.Point({
       lat: 40.509622849596695,
@@ -15,4 +14,34 @@ test('it works', function(assert) {
 
   assert.equal(result.lat(), 40.509622849596695);
   assert.equal(result.lng(), -75.5474853515625);
+});
+
+test('polygon', function(assert) {
+  var feature = new google.maps.Data.Feature({
+    geometry: new google.maps.Data.Polygon([
+      [
+        { lat: 42.52677220056902, lng: -71.114501953125 },
+        { lat: 42.4923525914282, lng: -71.14471435546875 },
+        { lat: 42.49336520339777, lng: -71.06369018554688 }
+      ]
+    ])
+  });
+  var result = featureCenter(feature);
+
+  assert.equal(result.lat(), 42.509562395998614);
+  assert.equal(result.lng(), -71.10420227050781);
+});
+
+test('linestring', function(assert) {
+  var feature = new google.maps.Data.Feature({
+    geometry: new google.maps.Data.LineString([
+      { lat: 42.5338562378485, lng: -71.15982055664062 },
+      { lat: 42.5338562378485, lng: -71.05545043945312 },
+      { lat: 42.503490421367296, lng: -71.08291625976562 }
+    ])
+  });
+  var result = featureCenter(feature);
+
+  assert.equal(result.lat(), 42.51867332960789);
+  assert.equal(result.lng(), -71.10763549804688);
 });
--- tests/unit/utils/get-measurement-test.js
+++ tests/unit/utils/get-measurement-test.js
@@ -3,23 +3,35 @@ import { module, test } from 'qunit';
 
 module('Unit | Utility | get measurement');
 
-// Replace this with your real tests.
 test('polyline', function(assert) {
-  var feature = mockFeature();
+  var feature = new google.maps.Data.Feature({
+    geometry: new google.maps.Data.LineString([
+      { lat: 42.5338562378485, lng: -71.15982055664062 },
+      { lat: 42.5338562378485, lng: -71.05545043945312 },
+      { lat: 42.503490421367296, lng: -71.08291625976562 }
+    ])
+  });
   var result = getMeasurement('polyline', feature);
+
   assert.ok(result.value);
   assert.ok(result.unit);
   assert.ok(result.measurementType);
 });
 
-function mockFeature() {
-  return {
-    getGeometry() {
-      return {
-        getArray() {
-          return [];
-        }
-      };
-    }
-  };
-}
+test('polygon', function(assert) {
+  var feature = new google.maps.Data.Feature({
+    geometry: new google.maps.Data.Polygon([
+      [
+        { lat: 42.52677220056902, lng: -71.114501953125 },
+        { lat: 42.4923525914282, lng: -71.14471435546875 },
+        { lat: 42.49336520339777, lng: -71.06369018554688 }
+      ]
+    ])
+  });
+  var result = getMeasurement('polygon', feature);
+
+  assert.ok(result.value);
+  assert.ok(result.unit);
+  assert.ok(result.measurementType);
+});
+
