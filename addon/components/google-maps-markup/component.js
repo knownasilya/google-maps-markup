0758db172c2e356a5458211e16736f2bf7ba1863
Ganesh Ughade <ganesh@webcontentor.com>
2017-05-01 20:34:11 +0530

    bring forward to the top any markup when you hover over it in the list

--- addon/components/google-maps-markup/component.js
+++ addon/components/google-maps-markup/component.js
@@ -433,15 +433,16 @@ export default Ember.Component.extend(ParentMixin, {
         style = {
           icon: {
             url: 'google-maps-markup/images/spotlight-poi-highlighted_hdpi.png',
-            scaledSize: new google.maps.Size(22, 40)
+            scaledSize: new google.maps.Size(22, 40),
           }
         };
       } else if (data.type === 'text') {
         data.feature.highlight();
       } else {
-        style = {
-          strokeColor: 'red'
-        };
+          style = {
+            strokeColor: 'red',
+            zIndex:99999
+          };
       }
 
       if (data.label) {
