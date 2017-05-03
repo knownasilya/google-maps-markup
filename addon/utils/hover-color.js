5e8fab4ad1a425fb57a25965b890dd2e50e5a7c4
Ganesh Ughade <ganesh@webcontentor.com>
2017-05-03 13:21:56 +0530

    jsHint error removed from marker icon

--- addon/utils/hover-color.js
+++ addon/utils/hover-color.js
@@ -1,14 +1,12 @@
-import Ember from 'ember';
+
 /* hexToComplimentary : Converts hex value to HSL, shifts
  * hue by 180 degrees and then converts hex, giving complimentary color
  * as a hex value
  * @param  [String] hex : hex value  
  * @return [String] : complimentary color as hex value
  */
-    // Credit to Denis http://stackoverflow.com/a/36253499/4939630
 export default function hoverColor(hex) {
     // Convert hex to rgb
-    // Credit to Denis http://stackoverflow.com/a/36253499/4939630
     var rgb = 'rgb(' + (hex = hex.replace('#', '')).match(new RegExp('(.{' + hex.length/3 + '})', 'g')).map(function(l) { return parseInt(hex.length%2 ? l+l : l, 16); }).join(',') + ')';
 
     // Get array of RGB values
@@ -17,7 +15,6 @@ export default function hoverColor(hex) {
     var r = rgb[0], g = rgb[1], b = rgb[2];
 
     // Convert RGB to HSL
-    // Adapted from answer by 0x000f http://stackoverflow.com/a/34946092/4939630
     r /= 255.0;
     g /= 255.0;
     b /= 255.0;
@@ -25,19 +22,19 @@ export default function hoverColor(hex) {
     var min = Math.min(r, g, b);
     var h, s, l = (max + min) / 2.0;
 
-    if(max == min) {
+    if(max === min) {
         h = s = 0;  //achromatic
     } else {
         var d = max - min;
         s = (l > 0.5 ? d / (2.0 - max - min) : d / (max + min));
 
-        if(max == r && g >= b) {
+        if(max === r && g >= b) {
             h = 1.0472 * (g - b) / d ;
-        } else if(max == r && g < b) {
+        } else if(max === r && g < b) {
             h = 1.0472 * (g - b) / d + 6.2832;
-        } else if(max == g) {
+        } else if(max === g) {
             h = 1.0472 * (b - r) / d + 2.0944;
-        } else if(max == b) {
+        } else if(max === b) {
             h = 1.0472 * (r - g) / d + 4.1888;
         }
     }
@@ -50,16 +47,25 @@ export default function hoverColor(hex) {
     h /= 360;
 
     // Convert h s and l values into r g and b values
-    // Adapted from answer by Mohsen http://stackoverflow.com/a/9493060/4939630
     if(s === 0){
         r = g = b = l; // achromatic
     } else {
         var hue2rgb = function hue2rgb(p, q, t){
-            if(t < 0) t += 1;
-            if(t > 1) t -= 1;
-            if(t < 1/6) return p + (q - p) * 6 * t;
-            if(t < 1/2) return q;
-            if(t < 2/3) return p + (q - p) * (2/3 - t) * 6;
+            if(t < 0) {
+                t += 1;
+            }
+            if(t > 1){
+                t -= 1;
+            }
+            if(t < 1/6) {
+                return p + (q - p) * 6 * t;
+            }
+            if(t < 1/2) {
+                return q;
+            }
+            if(t < 2/3) {
+            return p + (q - p) * (2/3 - t) * 6;
+            }
             return p;
         };
 
