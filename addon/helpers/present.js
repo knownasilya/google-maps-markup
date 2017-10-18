b9bb7f3c6eceee1312325536a961bdc822a72a4d
Ilya Radchenko <ilya@burstcreations.com>
2017-10-18 07:35:13 -0400

    fix: remove unused helper, add tool-id test

--- addon/helpers/present.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function present([data, key]) {
-  return key in data;
-}
-
-export default helper(present);
--- tests/unit/helpers/tool-id-test.js
+++ tests/unit/helpers/tool-id-test.js
@@ -4,9 +4,13 @@ import { module, test } from 'qunit';
 
 module('Unit | Helper | tool id');
 
-// Replace this with your real tests.
-test('it works', function(assert) {
-  let result = toolId([42]);
-  assert.ok(result);
+test('it works with id', function(assert) {
+  let result = toolId([{ id: 'test' }]);
+  assert.equal(result, 'test');
+});
+
+test('it works with type', function(assert) {
+  let result = toolId([{ type: 'test' }]);
+  assert.equal(result, 'test');
 });
 
