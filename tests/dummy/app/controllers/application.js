fc7c9035700904df6c1d01d15c44f9f86f5db3bc
Ilya Radchenko <ilya@burstcreations.com>
2018-06-01 13:53:43 -0400

    fix: update the deps, add load and save to dummy app

---
+++ tests/dummy/app/controllers/application.js
@@ -0,0 +1,52 @@
+import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { all, resolve } from 'rsvp';
+
+export default Controller.extend({
+  markupDataService: service('markupData'),
+
+  exportMarkup() {
+    let dataLayers = this.get('markupDataService.layers');
+
+    if (dataLayers) {
+      let promises = dataLayers.map(layer => {
+        return layer.toGeoJson();
+      });
+
+      return all(promises)
+        .then(layers => {
+          // make sure empty feature collections aren't exported
+          this.set('exported', layers);
+          return layers;
+        });
+    } else {
+      return resolve();
+    }
+  },
+
+  loadMarkup(markupData) {
+    let markupService = this.get('markupDataService');
+    let dataLayers = markupService.get('layers');
+
+    dataLayers.forEach((layer, index) => {
+      layer.data.addGeoJson(markupData[index]);
+      layer.data.forEach(feature => markupService.featureToResult(feature, layer));
+    });
+
+    markupService.changeModeByResults();
+  },
+
+  actions: {
+    load() {
+      let exported = this.get('exported');
+
+      if (exported) {
+        this.loadMarkup(exported);
+      }
+    },
+
+    save() {
+      this.exportMarkup();
+    }
+  }
+});
