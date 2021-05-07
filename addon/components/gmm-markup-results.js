ab18a13bfd7fbbd68239b79e24ade9a91206a0a4
Ilya Radchenko <knownasilya@gmail.com>
2021-05-06 21:34:47 -0400

    fix: convert results component to glimmer component

---
+++ addon/components/gmm-markup-results.js
@@ -0,0 +1,14 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+export default class GmmMarkupResults extends Component {
+  @action
+  sortEndAction() {
+    let results = this.args.results;
+
+    results.forEach((result) => {
+      result.style.zIndex = 0;
+    });
+    results[0].style.zIndex = 111;
+  }
+}
---
+++ app/components/gmm-markup-results.js
@@ -0,0 +1,1 @@
+export { default } from 'google-maps-markup/components/gmm-markup-results';
